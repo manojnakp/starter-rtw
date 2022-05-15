95e22f23e2a384730ceaff687cdec522ec57d2dc
Manojna <manojnakp2003@gmail.com>
2022-05-15 12:12:01 +0530

    Setup react
    Install react, react-dom and setup jsx compilation using babel and webpack.

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div className="text-center p2">
+      <h1 className="underline font-bold text-xl">Hello World!</h1>
+    </div>
+  );
+}
